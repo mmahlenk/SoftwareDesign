ee53a74aaa1edd7f50cb1bb5b2e26de1f5ba5d84
mmahlenk <mmahlenk@wellesley.edu>
2013-09-23 22:19:19 -0400

    Code for Exercise 3.5

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,6 +1,20 @@
 """Solution to an exercise in Think Python.
 
-Author:
+Author: Marisa Mahlenkamp
 """
 
-print '+ - - - -'
+def grid_ends():
+	print '+ - - - - + - - - - +'
+
+def grid_middle2():
+	for i in range(4):
+		print '|','       ','|','       ','|'
+	grid_ends() 
+
+def grid_total(n):
+	"""creates box with height n"""
+	grid_ends()
+	for i in range(n):
+		grid_middle2()
+
+grid_total(2)
