52bd4d65ed3141d3b0dd1270188f4e4144b300f9
mmahlenk <mmahlenk@wellesley.edu>
2013-09-24 23:51:16 -0400

    Adding Chap04 Exercises

---
+++ chap04/polygon.py
@@ -0,0 +1,47 @@
+"""Chapter 4 Exercises
+Author: Marisa Mahlenkamp
+"""
+
+from swampy.TurtleWorld import *
+import math
+
+world = TurtleWorld()
+bob = Turtle()
+print bob
+
+def square(t, length):
+	for i in range(length):
+		fd(t, 100)
+		lt(t)
+
+# square(bob, 3)
+
+def polygon(t, length, n):
+	for i in range(n):
+		fd(t, length)
+		lt(t, 360.0/n)
+
+# polygon(bob, 20, 15)
+
+def circle(t, r):
+	circumference = 2 * math.pi * r
+	n = int(circumference / 3) + 1
+	length = circumference / n
+	polygon(t, length, n)
+
+bob.delay = 0.01
+
+circle(bob, 40)
+
+def arc(t, r, angle):
+	circum = (2 * math.pi * r) * (angle / 360.0)
+	n = int(circum / 3) + 1
+	arc_length = circum / float(n)
+	arc_angle = angle / float(n)
+	for i in range(n):
+		fd(t, arc_length)
+		lt(t, arc_angle)
+
+arc(bob, 40, 180)	
+
+wait_for_user()
