985fa76151e1e169e12967d32214e9edba7ea458
mmahlenk <mmahlenk@wellesley.edu>
2013-10-01 17:19:52 -0400

    Updating code for Koch curve

--- chap05/koch.py
+++ chap05/koch.py
@@ -10,6 +10,15 @@ bob = Turtle()
 print bob
 
 def koch(t, length, n):
+	"""Function that creates image of a Koch curve.
+
+	t: Turtle
+	length: positive number
+	n: positive integer
+
+	returns: Turtle movement in shape of Koch curve. 
+	"""
+
 	angle = 60
 	if n == 0:
 		fd(t, length)
@@ -23,6 +32,14 @@ def koch(t, length, n):
 	koch(t, length/3.0, n-1)
 
 def snowflake(t, length, n):
+	"""Function that uses three Koch curves to create image of a snowflake.
+
+	t: Turtle
+	length: positive number
+	n: positive integer
+
+	returns: Turtle movements in shape of a snowflake.
+	"""
 	koch(t, length, n)
 	rt(t, 120)
 	koch(t, length, n)
