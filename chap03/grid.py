24ff065b6a1cba77a608a5e6d4d44d720e5ca374
Marisa Mahlenkamp <mmahlenk@wellesley.edu>
2013-10-30 21:57:45 -0400

    Fixing HW 3

--- chap03/grid.py
+++ chap03/grid.py
@@ -3,38 +3,25 @@
 Author: Marisa Mahlenkamp
 """
 
-def grid_ends():
-	print '+ - - - - + - - - - +'
+def grid_ends(length):
+	print '+ - - - - ' * length + '+'
 
-def grid_middle2():
+
+def grid_middle(length):
 	for i in range(4):
-		print '|','       ','|','       ','|'
-	grid_ends() 
+		print '|         ' * length + '|'
+	grid_ends(length) 
 
-def grid_total(n):
-	"""creates box with height n and length 2"""
-	grid_ends()
+
+def grid_total(n, length):
+	"""creates box with height n and width length"""
+	grid_ends(length)
 	for i in range(n):
-		grid_middle2()
+		grid_middle(length)
 
-
-
-def grid_ends_4():
-    print '+ - - - - + - - - - + - - - - + - - - - +'
-
-def grid_middle_4():
-    for i in range(4):
-        print '|','       ','|','       ','|','       ','|','       ','|'
-    grid_ends_4() 
-
-def grid_total_4(n):
-    """creates box with height n and length 4"""
-    grid_ends_4()
-    for i in range(n):
-        grid_middle_4()
 
 
 if __name__ == "__main__":
-    grid_total(2)
-    grid_total_4(4)
+    grid_total(2, 2)
+    grid_total(4, 4)
 
